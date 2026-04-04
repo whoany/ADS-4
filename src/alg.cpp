d02230cf4b83160bb36cff0cbfec21ff00c118c5
whoany <120975118+whoany@users.noreply.github.com>
2026-04-04 15:37:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,27 @@
-// Copyright 2021 NNTU-CS
-int countPairs1(int* arr, int len, int value) {
+int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len - 1; i++) {
-        if (arr[i] > value)
-            break;
         for (int j = i + 1; j < len; j++) {
-            int sum = arr[i] + arr[j];
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+int countPairs2(int *arr, int len, int value) {
+    int count = 0;
+    for (int left = 0; left < len - 1; left++) {
+        if (arr[left] > value) {
+            break;
+        }
+        for (int right = len - 1; right > left;
+             right--) {
+            int sum = arr[left] + arr[right];
             if (sum == value) {
                 count++;
-            } else if (sum > value) {
+            } else if (sum < value) {
                 break;
             }
         }
@@ -16,44 +29,8 @@ int countPairs1(int* arr, int len, int value) {
     return count;
 }
 
-int countPairs2(int* arr, int len, int value) {
-    int count = 0;
-    int left = 0;
-    int right = len - 1;
-
-    while (left < right) {
-        int sum = arr[left] + arr[right];
-
-        if (sum < value) {
-            left++;
-        } else if (sum > value) {
-            right--;
-        } else {
-            if (arr[left] == arr[right]) {
-                int n = right - left + 1;
-                count += n * (n - 1) / 2;
-                break;
-            }
-            int cntL = 1;
-            while (left + cntL < right
-                && arr[left + cntL] == arr[left]) {
-                cntL++;
-            }
-            int cntR = 1;
-            while (right - cntR > left
-                && arr[right - cntR] == arr[right]) {
-                cntR++;
-            }
-            count += cntL * cntR;
-            left += cntL;
-            right -= cntR;
-        }
-    }
-    return count;
-}
-
-static int bsFirst(int* arr, int from,
-    int to, int target) {
+static int bsFirst(int *arr, int from,
+                    int to, int target) {
     int result = -1;
     while (from <= to) {
         int mid = from + (to - from) / 2;
@@ -69,8 +46,8 @@ static int bsFirst(int* arr, int from,
     return result;
 }
 
-static int bsLast(int* arr, int from,
-    int to, int target) {
+static int bsLast(int *arr, int from,
+                   int to, int target) {
     int result = -1;
     while (from <= to) {
         int mid = from + (to - from) / 2;
@@ -86,20 +63,23 @@ static int bsLast(int* arr, int from,
     return result;
 }
 
-int countPairs3(int* arr, int len, int value) {
+int countPairs3(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len - 1; i++) {
         int target = value - arr[i];
-        if (target < 0)
+        if (target < 0) {
             continue;
-        if (arr[i] > value)
+        }
+        if (arr[i] > value) {
             break;
+        }
         int first = bsFirst(arr, i + 1,
-            len - 1, target);
-        if (first == -1)
+                             len - 1, target);
+        if (first == -1) {
             continue;
+        }
         int last = bsLast(arr, i + 1,
-            len - 1, target);
+                           len - 1, target);
         count += (last - first + 1);
     }
     return count;
