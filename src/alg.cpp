d10efd7449130cc114ef9d32382500bc0b5fecf9
whoany <120975118+whoany@users.noreply.github.com>
2026-04-04 16:09:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,104 +1,112 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len - 1; i++) {
-        for (int j = i + 1; j < len; j++) {
-            if (arr[i] + arr[j] == value) {
-                count++;
-            }
-        }
-    }
-    return count;
-}
+int countPairs1(int *data, int size, int target) {
+    int result = 0;
 
-int countPairs2(int *arr, int len, int value) {
-    int count = 0;
-
-    for (int left = 0; left < len - 1; left++) {
-        if (arr[left] > value) {
-            break;
-        }
-
-        int right = len - 1;
-
-        while (right > left &&
-               arr[left] + arr[right] > value) {
-            right--;
-        }
-
-        for (int j = right; j > left; j--) {
-            if (arr[left] + arr[j] == value) {
-                count++;
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (data[i] + data[j] == target) {
+                result++;
             }
         }
     }
 
-    return count;
-}
-
-static int bsFirst(int *arr, int from,
-                    int to, int target) {
-    int result = -1;
-    while (from <= to) {
-        int mid = from + (to - from) / 2;
-        if (arr[mid] == target) {
-            result = mid;
-            to = mid - 1;
-        } else if (arr[mid] < target) {
-            from = mid + 1;
-        } else {
-            to = mid - 1;
-        }
-    }
     return result;
 }
 
-static int bsLast(int *arr, int from,
-                   int to, int target) {
-    int result = -1;
-    while (from <= to) {
-        int mid = from + (to - from) / 2;
-        if (arr[mid] == target) {
-            result = mid;
-            from = mid + 1;
-        } else if (arr[mid] < target) {
-            from = mid + 1;
-        } else {
-            to = mid - 1;
+
+int countPairs2(int *data, int size, int target) {
+    int result = 0;
+
+    for (int i = 0; i < size - 1; i++) {
+
+        int needed = target - data[i];
+        int low = i + 1;
+        int high = size - 1;
+        int firstPos = -1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (data[mid] == needed) {
+                firstPos = mid;
+                high = mid - 1;
+            } else if (data[mid] < needed) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        if (firstPos != -1) {
+
+            int amount = 0;
+
+            for (int k = firstPos; k < size && data[k] == needed; k++) {
+                amount++;
+            }
+
+            if (data[i] == needed) {
+
+                int total = 0;
+                for (int k = i; k < size && data[k] == needed; k++) {
+                    total++;
+                }
+
+                result += total * (total - 1) / 2;
+                break;
+            } else {
+                result += amount;
+            }
         }
     }
+
     return result;
 }
 
-int countPairs3(int *arr, int len, int value) {
-    int count = 0;
 
-    for (int i = 0; i < len - 1; i++) {
-        int target = value - arr[i];
+int countPairs3(int *data, int size, int target) {
+    int result = 0;
 
-        if (arr[i] > value) {
-            break;
+    int leftIndex = 0;
+    int rightIndex = size - 1;
+
+    while (leftIndex < rightIndex) {
+
+        int currentSum = data[leftIndex] + data[rightIndex];
+
+        if (currentSum == target) {
+
+            if (data[leftIndex] == data[rightIndex]) {
+                int count = rightIndex - leftIndex + 1;
+                result += count * (count - 1) / 2;
+                break;
+            }
+
+            int leftCount = 1;
+            int rightCount = 1;
+
+            while (leftIndex + 1 < rightIndex &&
+                   data[leftIndex] == data[leftIndex + 1]) {
+                leftCount++;
+                leftIndex++;
+            }
+
+            while (rightIndex - 1 > leftIndex &&
+                   data[rightIndex] == data[rightIndex - 1]) {
+                rightCount++;
+                rightIndex--;
+            }
+
+            result += leftCount * rightCount;
+
+            leftIndex++;
+            rightIndex--;
+        } else if (currentSum < target) {
+            leftIndex++;
+        } else {
+            rightIndex--;
         }
-
-        int pos = bsFirst(arr, i + 1, len - 1, target);
-
-        if (pos == -1) {
-            continue;
-        }
-
-        int left = pos;
-        int right = pos;
-
-        while (left > i + 1 && arr[left - 1] == target) {
-            left--;
-        }
-
-        while (right < len - 1 && arr[right + 1] == target) {
-            right++;
-        }
-
-        count += (right - left + 1);
     }
 
-    return count;
+    return result;
 }
